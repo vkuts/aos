d00de3ffb9b556c1d4b9689eca7e32072f24c468
Vlad Kutsenko <vladdie@gmail.com>
2024-10-28 12:57:50 -0400

    added  a couple of basic lua modules to test using new lua logic inside of the WASM bundle; added a test suite to prove that they work

---
+++ process/fun/init.lua
@@ -0,0 +1,13 @@
+local fun = require(".fun.fun")
+
+-- local crypto = {
+--     _version = "0.0.1",
+--     digest = digest,
+--     utils = util,
+--     cipher = cipher,
+--     random = cipher.issac.random,
+--     mac = mac,
+--     kdf = kdf,
+-- };
+
+return fun
