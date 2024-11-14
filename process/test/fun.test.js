64a6dd5fd03eb93985a4db560956ad7b3e43bbeb
Vlad Kutsenko <vladdie@gmail.com>
2024-11-13 22:16:55 -0500

    added simple Rust lib utils that work with strings

--- process/test/fun.test.js
+++ process/test/fun.test.js
@@ -6,8 +6,103 @@ import fs from 'fs'
 const wasm = fs.readFileSync('./process.wasm')
 const options = {
 	format: "wasm32-unknown-emscripten4", // wasm64-unknown-emscripten-draft_2024_02_15
-	extensions: [],
  }
+
+ test('Use vlad_rusty to get ohai', async () => {
+  const handle = await AoLoader(wasm, options);
+	const env = {
+		Process: {
+			Id: 'AOS',
+			Owner: 'FOOBAR',
+			Tags: [{ name: 'Name', value: 'Thomas' }],
+		},
+	};
+
+	const data = `
+    local result = get_ohai()
+		return result
+	`;
+	const msg = {
+		Target: 'AOS',
+		From: 'FOOBAR',
+		Owner: 'FOOBAR',
+		['Block-Height']: '1000',
+		Id: '1234xyxfoo',
+		Module: 'WOOPAWOOPA',
+		Tags: [{ name: 'Action', value: 'Eval' }],
+		Data: data,
+	};
+
+	const result = await handle(null, msg, env);
+
+  assert.equal(result.Error, undefined)
+  assert.equal(result.Output?.data, "ohai")
+  assert.ok(true)
+})
+
+test('Use vlad_rusty to say hi to a fren', async () => {
+  const handle = await AoLoader(wasm, options);
+	const env = {
+		Process: {
+			Id: 'AOS',
+			Owner: 'FOOBAR',
+			Tags: [{ name: 'Name', value: 'Thomas' }],
+		},
+	};
+
+	const data = `
+    local result = say_hi("Vic")
+		return result
+	`;
+	const msg = {
+		Target: 'AOS',
+		From: 'FOOBAR',
+		Owner: 'FOOBAR',
+		['Block-Height']: '1000',
+		Id: '1234xyxfoo',
+		Module: 'WOOPAWOOPA',
+		Tags: [{ name: 'Action', value: 'Eval' }],
+		Data: data,
+	};
+
+	const result = await handle(null, msg, env);
+
+  assert.equal(result.Error, undefined)
+  assert.equal(result.Output?.data, "ohai")
+  assert.ok(true)
+})
+
+//  test('Use didkit.add_two_integers successfully', async () => {
+//   const handle = await AoLoader(wasm, options);
+// 	const env = {
+// 		Process: {
+// 			Id: 'AOS',
+// 			Owner: 'FOOBAR',
+// 			Tags: [{ name: 'Name', value: 'Thomas' }],
+// 		},
+// 	};
+
+// 	const data = `
+//     local result = add_two_integers(3, 4)
+// 		return result
+// 	`;
+// 	const msg = {
+// 		Target: 'AOS',
+// 		From: 'FOOBAR',
+// 		Owner: 'FOOBAR',
+// 		['Block-Height']: '1000',
+// 		Id: '1234xyxfoo',
+// 		Module: 'WOOPAWOOPA',
+// 		Tags: [{ name: 'Action', value: 'Eval' }],
+// 		Data: data,
+// 	};
+
+// 	const result = await handle(null, msg, env);
+
+//   assert.equal(result.Error, undefined)
+//   assert.equal(result.Output?.data, "7")
+//   assert.ok(true)
+// })
 
 test('Use vlad_rusty.add_two_integers successfully', async () => {
   const handle = await AoLoader(wasm, options);
