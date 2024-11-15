ebf4383c8b395ceb2d6ef42a935500c63d867a9c
Vlad Kutsenko <vladdie@gmail.com>
2024-11-15 15:11:22 -0500

    added a simple C program linking to a simple Rust lib

---
+++ process/vlad_c/vlad_rusty_lib.h
@@ -0,0 +1,9 @@
+#ifndef VLAD_RUSTY_LIB_H
+#define VLAD_RUSTY_LIB_H
+
+int add_two_integers(int a, int b);
+int subtract_two_integers(int a, int b);
+char* get_ohai();
+char* say_hi(const char* name);
+
+#endif
